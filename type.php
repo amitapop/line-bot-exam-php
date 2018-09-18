2b42b5f9c8a08ff3c96ddc2eb7ae84480c0bcdea
amitapop <41670640+amitapop@users.noreply.github.com>
2018-09-18 16:13:32 +0700

    Update type.php

--- type.php
+++ type.php
@@ -39,7 +39,7 @@
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "location";
         $arrayPostData['messages'][0]['title'] = "หอพัก 40 ปี";
-        $arrayPostData['messages'][0]['address'] = "Chiang Mai University เมืองเชียงใหม่ เชียงใหม่ 50000 ประเทศไทย,18.7924210,99.0384200";
+        $arrayPostData['messages'][0]['address'] = "Chiang Mai University เมืองเชียงใหม่ เชียงใหม่ 50000 ประเทศไทย";
         $arrayPostData['messages'][0]['latitude'] = "18.7924210";
         $arrayPostData['messages'][0]['longitude'] = "99.0384200";
         replyMsg($arrayHeader,$arrayPostData);
